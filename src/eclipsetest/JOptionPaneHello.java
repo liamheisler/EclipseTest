19b04ba37586a9b721c9bef7b2424a2714f2f90c
Liam <liamh2017@gmail.com>
2015-02-07 14:26:25 -0800

    just a test

---
+++ src/eclipsetest/JOptionPaneHello.java
@@ -0,0 +1,11 @@
+package eclipsetest;
+
+import javax.swing.JOptionPane;
+
+public class JOptionPaneHello {
+
+	public static void main(String[] args)
+	{
+		JOptionPane.showMessageDialog(null, "Hello");
+	}
+}
